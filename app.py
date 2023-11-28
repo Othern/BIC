9540c3a23af77d309360f80eb5275f7459452c6e
Othern <willy910928@gmail.com>
2023-11-28 16:51:53 +0800

    text_voice_chatroom split

--- app.py
+++ app.py
@@ -6,14 +6,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 activate_room = {}
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        if 'create' in request.form:
+        if 'create_text' in request.form:
             # 生成唯一的房间代码
             room_code = uuid.uuid4().hex[:6].upper()
             activate_room[room_code] = []
-            return redirect(url_for('.chat', room=room_code))
+            return redirect(url_for('.chat', room=room_code,type='text'))
+        elif 'create_voice' in request.form:
+            # 生成唯一的房间代码
+            room_code = uuid.uuid4().hex[:6].upper()
+            activate_room[room_code] = []
+            return redirect(url_for('.chat', room=room_code,type='voice'))
+
         elif 'join' in request.form:
             # 加入已有的房间
             room_code = request.form.get('room_code')
@@ -23,9 +30,9 @@ def index():
                 return render_template('error_join.html',room=room_code)
     return render_template('index.html')
 
-@app.route('/chat/<room>')
-def chat(room):
-    return render_template('chat.html', room=room)
+@app.route('/chat_text/<room>/<type>')
+def chat(room,type):
+    return render_template('chat.html', room=room,type=type)
 
 @socketio.on('message')
 def handle_message(data):
@@ -37,7 +44,7 @@ def handle_message(data):
 
     send(data, room=room)
 
-@socketio.on('join')
+@socketio.on('text_join')
 def on_join(data):
     username = data['username']
     room = data['room']
@@ -58,4 +65,4 @@ def on_leave(data):
     send({'msg': username + ' has left the room.', 'user': 'System'}, room=room)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=4000, debug=True)
